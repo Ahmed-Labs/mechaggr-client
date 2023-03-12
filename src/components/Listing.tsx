6edf3b5492408d73552dbe98cadf4d7f1a9036ef
Ahmed Mohamed <ahmedelhagjr@gmail.com>
2023-03-11 23:59:13 -0500

    create listing component that accepts post data object

---
+++ src/components/Listing.tsx
@@ -0,0 +1,28 @@
+import { type NextPage } from "next";
+import React from "react";
+type postType = {
+  title: string;
+  author: string;
+  postType: string;
+  body: string;
+  dateCreated: Date;
+  url: string;
+};
+
+type listingProps = {
+  post: postType;
+  handleClick: React.Dispatch<React.SetStateAction<{}>>;
+};
+const Listing: NextPage<listingProps> = ({ post, handleClick }) => {
+  return (
+    <div
+      onClick={() => handleClick(post)}
+      className="cursor-pointer border-b border-gray-500 py-4 px-4 hover:bg-slate-900"
+    >
+      <h1>{post.title}</h1>
+      <p className="text-gray-300">{post.body.slice(0, 120)}...</p>
+    </div>
+  );
+};
+
+export default Listing;
