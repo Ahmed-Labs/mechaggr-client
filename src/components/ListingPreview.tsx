61afe534d8f58074716c460c5e1576c62ffa801e
Ahmed Mohamed <ahmedelhagjr@gmail.com>
2023-03-12 15:12:45 -0400

    displayed more information in listing preview and increase readability

--- src/components/ListingPreview.tsx
+++ src/components/ListingPreview.tsx
@@ -1,7 +1,8 @@
 import { type NextPage } from "next";
 import ReactMarkdown from "react-markdown";
 import remarkGfm from "remark-gfm";
-
+import { BiLinkExternal } from "react-icons/bi";
+import { ReactNode } from "react";
 type postType = {
   title: string;
   author: string;
@@ -12,15 +13,30 @@ type postType = {
 };
 
 type listingProps = {
-  post?: postType;
+  post: postType;
 };
-
+// https://api.imgur.com/post/v1/albums/G5KBar0?client_id=546c25a59c58ad7&include=media%2Cadconfig%2Caccount
 const ListingPreview: NextPage<listingProps> = ({ post }) => {
   return (
-    <div className="h-full w-full overflow-y-auto p-5">
-      {post && (
-        <ReactMarkdown children={post.body} remarkPlugins={[remarkGfm]} />
-      )}
+    <div className="flex h-full w-full flex-col">
+      <div className="flex items-center bg-[#273137] px-4 py-2 font-bold">
+        <div>
+          <h1>MechMarket</h1>
+          <a target="_blank" href={`https://www.reddit.com/user/${post.author}`}>
+            u/{post.author}
+          </a>
+        </div>
+        <a target="_blank" href={post.url} className="ml-auto">
+          <BiLinkExternal size={30} />
+        </a>
+      </div>
+
+      <div className="relative flex-1 overflow-y-auto p-2">
+        <div className="p-3">
+          <div className="mb-3 font-bold">{post.title}</div>
+          <ReactMarkdown children={post.body} remarkPlugins={[remarkGfm]} />
+        </div>
+      </div>
     </div>
   );
 };
