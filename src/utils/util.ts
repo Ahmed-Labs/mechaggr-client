efc4203d7f979a9c172ba0c4051a4f66beaa20c4
Ahmed Mohamed <ahmedelhagjr@gmail.com>
2023-03-17 01:19:14 -0400

    add utility function for getting the 'time ago' string from date object

---
+++ src/utils/util.ts
@@ -0,0 +1,22 @@
+export function getTimeAgoString(date: Date): string {
+  const seconds = Math.floor((new Date().getTime() - date.getTime()) / 1000);
+  const intervals = [
+    { label: "yr", seconds: 31536000 },
+    { label: "mon", seconds: 2592000 },
+    { label: "d", seconds: 86400 },
+    { label: "hr", seconds: 3600 },
+    { label: "min", seconds: 60 },
+    { label: "s", seconds: 1 },
+  ];
+
+  for (let i = 0; i < intervals.length; i++) {
+    const interval = intervals[i];
+    if (!interval) continue;
+    const count = Math.floor(seconds / interval.seconds);
+    if (count >= 1) {
+      return `${count} ${interval?.label}${count > 1 ? "s" : ""} ago`;
+    }
+  }
+
+  return "just now";
+}
