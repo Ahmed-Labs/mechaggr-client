628c1fb02abd16396071d63060ee3cc21d6ac2df
Ahmed Mohamed <ahmedelhagjr@gmail.com>
2023-05-06 14:36:14 -0400

    add post ID to post data to fix selected listing bug and key for mapping

--- src/components/Listing.tsx
+++ src/components/Listing.tsx
@@ -4,6 +4,7 @@ import { getTimeAgoString } from "~/utils/util";
 import { AiFillRedditCircle } from "react-icons/ai";
 import { isServerDefault } from "@trpc/server/dist/core/internals/config";
 type postType = {
+  id: string;
   title: string;
   author: string;
   postType: string;
@@ -15,20 +16,19 @@ type postType = {
 type listingProps = {
   post: postType;
   isSelected: boolean;
-  handleClick: React.Dispatch<React.SetStateAction<postType>>;
+  handleClick: React.Dispatch<React.SetStateAction<postType | null>>;
 };
 const Listing: NextPage<listingProps> = ({ post, isSelected, handleClick }) => {
-
-  
+    
   return (
     <div
       onClick={() => handleClick(post)}
-      className={`${isSelected? "border-b-2 border-white bg-[#0000004e]": ""} cursor-pointer border-b border-gray-500 py-4 px-4 hover:bg-[#0000004e]`}
+      className={`${isSelected? "bg-[#0000004e]": ""} cursor-pointer border-b border-[#ffffff36] py-4 px-4 hover:bg-[#0000004e] transition ease-in-out delay-75`}
     > 
-      <div className="flex">
+      <div className="flex items-center mb-2">
         <h1 className="font-bold">r/Mechmarket</h1>
-
-        <span className="ml-auto mb-2 text-gray-400">
+        {isSelected && <span className="bg-white justify-center w-2 h-2 rounded-full mx-2"></span>}
+        <span className="ml-auto text-gray-400">
           {getTimeAgoString(post.dateCreated)}
         </span>
       </div>
