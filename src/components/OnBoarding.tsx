9b53ea9c327c0e84632e0545c0a41b0971c1062f
Ahmed Mohamed <ahmedelhagjr@gmail.com>
2023-05-29 01:15:49 -0400

    handle new users upon entering using 'createUser' mutation

--- src/components/OnBoarding.tsx
+++ src/components/OnBoarding.tsx
@@ -1,34 +1,38 @@
-import React from "react";
-import { v4 as uuidv4 } from "uuid";
+import React, { useState } from "react";
 import { FiAlertTriangle } from "react-icons/fi";
+import { api } from "~/utils/api";
 
 type Props = {
   onClose: () => void;
 };
 
+function newUser() {
+  return api.userAlerts.createUser.useMutation();
+}
+
 const OnBoarding: React.FC<Props> = ({ onClose }) => {
-  function createUser() {
-    // User {
-    //     userId
-    //     alerts []
-    // }
-    const userId = uuidv4();
-    localStorage.setItem("userId", userId);
-    onClose();
+  const createUserMutation = api.userAlerts.createUser.useMutation();
+
+  async function handleNewUser() {
+    await createUserMutation.mutateAsync().then((res) => {
+      localStorage.setItem("userId", res.userId);
+      onClose();
+    });
   }
+
   return (
     <div className="flex h-screen w-full items-center justify-center">
-      <div className="flex w-[600px] flex-col rounded-lg bg-[#3f3f74] p-10">
-        <h1 className="mb-3 text-center text-xl font-semibold text-white">
-          Welcome to <span className="font-bold">mechfeed</span>!
+      <div className="flex w-[600px] flex-col rounded-lg border-2 border-[#5b5b71] bg-[#13161B] p-10 ">
+        <h1 className="mb-3 text-center text-4xl font-semibold text-[#eeeff0]">
+          <span className="font-bold">mechfeed</span>
         </h1>
-        <p className="text-lg text-[#eeeeff]">
+        <p className="text-lg text-[#b4bccb]">
           Keep missing items listed at great prices because someone always beats
-          you to it? Mechfeed allows you to monitor and receive alerts from all mechanical keyboard
-          markets in real-time.
+          you to it? Mechfeed allows you to monitor and receive alerts from all
+          mechanical keyboard markets in real-time.
         </p>
-        <div className="text-[#eeeeff] my-3">
-            <p className="text-white font-medium">Markets Monitored:</p>
+        <div className="my-3 text-[#b4bccb]">
+          <p className="font-medium text-[#c2cad8]">Markets Monitored:</p>
           <ul>
             <li>- MechMarket SubReddit</li>
             <li>- MechMarket Discord</li>
@@ -40,8 +44,8 @@ const OnBoarding: React.FC<Props> = ({ onClose }) => {
           </ul>
         </div>
         <div className="mb-3 rounded bg-[#dcdf80] px-3 py-4 ">
-          <p className="mx-1 flex">
-            <span className="mr-4 flex items-center">
+          <p className="mx-1 flex font-semibold">
+            <span className="mr-4 flex items-center ">
               <FiAlertTriangle size={20} />
             </span>{" "}
             If you clear site data at any point, you will lose your alerts and
@@ -49,8 +53,8 @@ const OnBoarding: React.FC<Props> = ({ onClose }) => {
           </p>
         </div>
         <button
-          onClick={createUser}
-          className="rounded bg-[#6464a4] px-2 py-2 text-white  transition duration-150 ease-in-out hover:bg-[#53538d]"
+          onClick={() => handleNewUser()}
+          className="rounded bg-[#282C34] px-2 py-2 text-[#c3cad6]  transition duration-150 ease-in-out hover:bg-[#3C424D]"
         >
           Enter
         </button>
